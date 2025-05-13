70f6a7e6f2bde00c7221003b4708ccbe2cc362b2
bhuism <bhuism@odee.net>
2025-05-14 00:15:58 +0200

    use createGetLoadContext

--- app/server/context.ts
+++ app/server/context.ts
@@ -1,5 +1,7 @@
 import type { Context } from "hono"
-// import { i18next } from "remix-hono/i18next"
+import { createGetLoadContext } from "react-router-hono-server/cloudflare"
+import { i18next } from "remix-hono/i18next"
+import { getClientEnv, getServerEnv } from "~/env.server"
 // import { getClientEnv, getServerEnv } from "~/env.server"
 
 // const createGetLoadContext = async (c: Context) => {
@@ -24,36 +26,49 @@ import type { Context } from "hono"
 // 	}
 // }
 
-// export const getLoadContext = createGetLoadContext((_c, { mode, build }) => {
-// 	const isProductionMode = mode === "production"
-// 	return {
-// 		appVersion: isProductionMode ? build.assets.version : "dev",
-// 	}
-// })
+export const getLoadContext = createGetLoadContext(async (c, { mode, build }) => {
+	// biome-ignore lint/suspicious/noConsole: <explanation>
+	// biome-ignore lint/style/useTemplate: <explanation>
+	console.log("mode=" + mode)
 
-//import type { Context } from "hono"
-import { i18next } from "remix-hono/i18next"
-import { getClientEnv, getServerEnv } from "~/env.server"
-
-export const getLoadContext = async (c: Context) => {
-	// get the locale from the context
-	const locale = i18next.getLocale(c)
-	// get t function for the default namespace
-	const t = await i18next.getFixedT(c)
-	// get the server environment
+	const locale = i18next.getLocale(c as Context)
+	const t = await i18next.getFixedT(c as Context)
 	const env = getServerEnv(c.env)
+
+	const isProductionDeployment = mode === "production"
 
 	return {
 		lang: locale,
 		t,
-		isProductionDeployment: env.APP_ENV === "production",
+		isProductionDeployment,
 		env,
 		clientEnv: getClientEnv(),
-		//		appVersion: env.APP_ENV === "production" ? build.assets.version : "dev",
-		// We do not add this to AppLoadContext type because it's not needed in the loaders, but it's used above to handle requests
 		body: c.body,
+		appVersion: isProductionDeployment ? build.assets.version : "dev",
 	}
-}
+})
+
+//import type { Context } from "hono"
+
+// export const getLoadContext = async (c: Context) => {
+// 	// get the locale from the context
+// 	const locale = i18next.getLocale(c)
+// 	// get t function for the default namespace
+// 	const t = await i18next.getFixedT(c)
+// 	// get the server environment
+// 	const env = getServerEnv(c.env)
+
+// 	return {
+// 		lang: locale,
+// 		t,
+// 		isProductionDeployment: env.APP_ENV === "production",
+// 		env,
+// 		clientEnv: getClientEnv(),
+// 		//		appVersion: env.APP_ENV === "production" ? build.assets.version : "dev",
+// 		// We do not add this to AppLoadContext type because it's not needed in the loaders, but it's used above to handle requests
+// 		body: c.body,
+// 	}
+// }
 
 // interface LoadContext extends Awaited<ReturnType<typeof getLoadContext>> {}
 
@@ -63,11 +78,23 @@ export const getLoadContext = async (c: Context) => {
 // declare module "react-router" {
 // 	interface AppLoadContext extends Omit<LoadContext, "body"> {}
 // }
-interface LoadContext extends Awaited<ReturnType<typeof getLoadContext>> {}
+//interface LoadContext extends Awaited<ReturnType<typeof getLoadContext>> {}
+
+// /**
+//  * Declare our loaders and actions context type
+//  */
+// declare module "react-router" {
+// 	interface AppLoadContext extends Omit<LoadContext, "body"> {}
+// }
 
 /**
  * Declare our loaders and actions context type
  */
 declare module "react-router" {
-	interface AppLoadContext extends Omit<LoadContext, "body"> {}
+	interface AppLoadContext {
+		readonly lang: string
+		readonly appVersion: string
+	}
 }
+
+//interface LoadContext extends Awaited<ReturnType<typeof getLoadContext>> {}
