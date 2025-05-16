ec144ddedbd1f17f61f8d685bb5fea59aaddecad
Bas <bhuism@users.noreply.github.com>
2025-05-16 23:41:44 +0200

    Update HomePage.tsx

--- app/routes/HomePage.tsx
+++ app/routes/HomePage.tsx
@@ -1,11 +1,8 @@
 import LoginButton from "~/components/LoginButton"
 import CenterComponents from "~/utils/CenterComponents"
-import constants from "~/utils/constants"
-import type { Route } from "./+types/HomePage"
-
 import Star from "~/layout/Star"
 
-const Home: React.FC<Route.ComponentProps> = () => {
+const Home: React.FC = () => {
 	return (
 		<CenterComponents>
 			<Star />
