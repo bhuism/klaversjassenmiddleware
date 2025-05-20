52cdaa884088ae9b4de274fed951d1582712e129
Bas Huisman <bhuism@odee.net>
2025-05-20 15:55:32 +0200

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,43 @@
 import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
+import { minimal2023Preset as preset, defineConfig as pwaDefineConfig } from "@vite-pwa/assets-generator/config"
 import { reactRouterDevTools } from "react-router-devtools"
 import { defineConfig } from "vite"
+import { VitePWA, type VitePWAOptions } from "vite-plugin-pwa"
 import tsconfigPaths from "vite-tsconfig-paths"
+
+const pwaAssets = pwaDefineConfig({
+	preset,
+	images: ["public/*"],
+})
+
+const pwaOptions: Partial<VitePWAOptions> = {
+	base: "/",
+	registerType: "autoUpdate",
+	includeAssets: ["favicon.png"],
+	injectRegister: "inline",
+	outDir: "build/client",
+	manifest: {
+		name: "Klavers Jassen",
+		short_name: "Klavers Jassen",
+		description: "Klavers Jassen",
+		theme_color: "#000000",
+		background_color: "#000000",
+		start_url: ".",
+	},
+	pwaAssets: pwaAssets,
+	workbox: {
+		globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+		cleanupOutdatedCaches: true,
+		clientsClaim: true,
+	},
+	devOptions: {
+		enabled: true,
+		navigateFallback: "index.html",
+		suppressWarnings: false,
+		type: "module",
+	},
+}
 
 export default defineConfig({
 	build: {
@@ -16,7 +51,7 @@ export default defineConfig({
 	define: {
 		__DATE__: `'${new Date().toISOString()}'`,
 	},
-	plugins: [tailwindcss(), reactRouterDevTools(), reactRouter(), tsconfigPaths()],
+	plugins: [tailwindcss(), VitePWA(pwaOptions), reactRouterDevTools(), reactRouter(), tsconfigPaths()],
 	server: {
 		open: true,
 		// biome-ignore lint/nursery/noProcessEnv: Its ok to use process.env here
