5cabfca45bcc236e01592d70c95c030cae1c41b5
Bas Huisman <bhuism@odee.net>
2025-05-20 21:32:32 +0200

    better menu

--- app/components/MainMenu.tsx
+++ app/components/MainMenu.tsx
@@ -19,73 +19,82 @@ const MainMenu = () => {
 		setAnchorElNav(null)
 	}
 
-	const menu: MyMenu[] =
-		isAuthenticated && !isLoading
-			? [
-					{ id: "games", title: "Games", onClick: () => navigate("/games") },
-					{
-						id: "messageboard",
-						title: "Message Board",
-						onClick: () => navigate("/messageboard"),
-					},
-				]
-			: []
+	const menu: MyMenu[] = [
+		{ id: "games", title: "Games", onClick: () => navigate("/games") },
+		{
+			id: "messageboard",
+			title: "Message Board",
+			onClick: () => navigate("/messageboard"),
+		},
+	]
 
 	return (
 		<>
 			<Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-				<IconButton
-					size="large"
-					aria-label="account of current user"
-					aria-controls="menu-appbar-main"
-					aria-haspopup="true"
-					onClick={handleOpenNavMenu}
-					color="inherit"
-				>
-					<MenuIcon />
-				</IconButton>
-				<Menu
-					id="menu-appbar-main"
-					anchorEl={anchorElNav}
-					anchorOrigin={{
-						vertical: "bottom",
-						horizontal: "left",
-					}}
-					keepMounted
-					transformOrigin={{
-						vertical: "top",
-						horizontal: "left",
-					}}
-					open={Boolean(anchorElNav)}
-					onClose={handleCloseNavMenu}
-					sx={{ display: { xs: "block", md: "none" } }}
-				>
-					{menu.map((item) => (
-						<MenuItem
-							key={item.id}
-							onClick={() => {
-								handleCloseNavMenu()
-								item.onClick()
-							}}
+				{isAuthenticated && !isLoading ? (
+					<>
+						<IconButton
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar-main"
+							aria-haspopup="true"
+							onClick={handleOpenNavMenu}
+							color="inherit"
 						>
-							<Typography sx={{ textAlign: "center" }}>{item.title}</Typography>
-						</MenuItem>
-					))}
-				</Menu>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id="menu-appbar-main"
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: "bottom",
+								horizontal: "left",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "left",
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{ display: { xs: "block", md: "none" } }}
+						>
+							{menu.map((item) => (
+								<MenuItem
+									key={item.id}
+									onClick={() => {
+										handleCloseNavMenu()
+										item.onClick()
+									}}
+								>
+									<Typography sx={{ textAlign: "center" }}>{item.title}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</>
+				) : (
+					<></>
+				)}
 			</Box>
 
 			<Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-				{menu.map((page) => (
-					<Button
-						key={page.id}
-						sx={{ my: 2, color: "white", display: "block" }}
-						onClick={handleCloseNavMenu}
-						component={Link}
-						to={page.id}
-					>
-						{page.title}
-					</Button>
-				))}
+				{isAuthenticated && !isLoading ? (
+					<>
+						{menu.map((page) => (
+							<Button
+								key={page.id}
+								sx={{ my: 2, color: "white", display: "block" }}
+								onClick={handleCloseNavMenu}
+								component={Link}
+								to={page.id}
+							>
+								{page.title}
+							</Button>
+						))}
+					</>
+				) : (
+					<></>
+				)}
 			</Box>
 		</>
 	)
