96cc63beff36b5241358b4b2b137bf8dc1d8fa3e
bhuism <bhuism@odee.net>
2025-05-23 12:29:10 +0200

    use react query

--- app/components/game/PlayerName.tsx
+++ app/components/game/PlayerName.tsx
@@ -1,30 +1,23 @@
+import { useQuery } from "@tanstack/react-query"
 import useGameApi from "~/hooks/useGameApi"
-import useLoadOnce from "~/hooks/useLoadOnce"
-
-// export const getPlayerName = (cardApi: GameApi, uid: string): Promise<string> => {
-// 	const cacheName = names.get(uid)
-
-// 	if (!cacheName) {
-// 		return cardApi.getUser(uid).then((u) => u.displayName)
-// 	}
-
-// 	return Promise.resolve(cacheName)
-// }
 
 const PlayerName: React.FC<React.PropsWithChildren<{ playerUid: string }>> = ({ playerUid }) => {
 	const { cardApi } = useGameApi()
 
-	const { data, error, isLoading } = useLoadOnce(() => cardApi.getUser(playerUid))
+	const { isPending, error, data } = useQuery({
+		queryKey: [playerUid],
+		queryFn: () => cardApi.getUser(playerUid),
+	})
 
 	if (error) {
 		return <>{`${playerUid}`}</>
 	}
 
-	if (isLoading) {
-		return <span className="blink">loading</span>
+	if (isPending) {
+		return <span className="blink">{`${playerUid}`}</span>
 	}
 
-	return <>{`${data ? data.displayName : playerUid}`}</>
+	return <>{`${data.displayName}`}</>
 }
 
 export default PlayerName
