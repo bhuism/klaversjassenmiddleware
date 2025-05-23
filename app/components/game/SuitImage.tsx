b9fb693948e7d583e14744077306e28d19408f1f
bhuism <bhuism@odee.net>
2025-05-23 11:57:38 +0200

    suit image

--- app/components/game/SuitImage.tsx
+++ app/components/game/SuitImage.tsx
@@ -4,17 +4,48 @@ import { Suit } from "../common/enum"
 
 const ClubIcon = createSvgIcon(
 	// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M11.5 12.5a3.5 3.5 0 0 1-2.684-1.254 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5" />
+	</svg>,
+	"Club"
+)
+
+const DiamondIcon = createSvgIcon(
+	// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
 		width="16"
 		height="16"
 		fill="currentColor"
-		className="bi bi-suit-club-fill"
 		viewBox="0 0 16 16"
+		style={{ color: "red" }}
 	>
-		<path d="M11.5 12.5a3.5 3.5 0 0 1-2.684-1.254 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907 3.5 3.5 0 1 1-2.538-5.743 3.5 3.5 0 1 1 6.708 0A3.5 3.5 0 1 1 11.5 12.5" />
+		<path d="M2.45 7.4 7.2 1.067a1 1 0 0 1 1.6 0L13.55 7.4a1 1 0 0 1 0 1.2L8.8 14.933a1 1 0 0 1-1.6 0L2.45 8.6a1 1 0 0 1 0-1.2" />
 	</svg>,
-	"Club"
+	"Diamond"
+)
+
+const HeartIcon = createSvgIcon(
+	// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="16"
+		height="16"
+		fill="currentColor"
+		viewBox="0 0 16 16"
+		style={{ color: "red" }}
+	>
+		<path d="M4 1c2.21 0 4 1.755 4 3.92C8 2.755 9.79 1 12 1s4 1.755 4 3.92c0 3.263-3.234 4.414-7.608 9.608a.513.513 0 0 1-.784 0C3.234 9.334 0 8.183 0 4.92 0 2.755 1.79 1 4 1" />
+	</svg>,
+	"Heart"
+)
+
+const SpadeIcon = createSvgIcon(
+	// biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
+	<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+		<path d="M7.184 11.246A3.5 3.5 0 0 1 1 9c0-1.602 1.14-2.633 2.66-4.008C4.986 3.792 6.602 2.33 8 0c1.398 2.33 3.014 3.792 4.34 4.992C13.86 6.367 15 7.398 15 9a3.5 3.5 0 0 1-6.184 2.246 20 20 0 0 0 1.582 2.907c.231.35-.02.847-.438.847H6.04c-.419 0-.67-.497-.438-.847a20 20 0 0 0 1.582-2.907" />
+	</svg>,
+	"Spade"
 )
 
 const SuitImage: React.FC<{ suit: Suit }> = ({ suit }) => {
@@ -22,11 +53,11 @@ const SuitImage: React.FC<{ suit: Suit }> = ({ suit }) => {
 		case Suit.Clubs:
 			return <ClubIcon />
 		case Suit.Diamonds:
-			return <ClubIcon />
+			return <DiamondIcon />
 		case Suit.Hearts:
-			return <ClubIcon />
+			return <HeartIcon />
 		case Suit.Spades:
-			return <ClubIcon />
+			return <SpadeIcon />
 	}
 }
 
