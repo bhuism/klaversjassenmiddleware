bf7a0e621c9cab2b8bca72bf0128e82f872bd02c
bhuism <bhuism@odee.net>
2025-06-06 12:37:23 +0200

    cleanups

--- app/components/GamePlay.tsx
+++ app/components/GamePlay.tsx
@@ -1,11 +1,32 @@
-import { Typography } from "@mui/material"
+import { CircularProgress, Typography } from "@mui/material"
+import { Suspense, lazy } from "react"
+import Logo192 from "~/layout/Logo192"
 import type { GameState } from "~/types"
+import CenterComponents from "~/utils/CenterComponents"
+
+const PhaserComponent = lazy(() =>
+	import("./PhaserComponent").then((module) => ({
+		default: module.default,
+	}))
+)
+
+const LoadingGame: React.FC<{ game: GameState }> = ({ game }) => {
+	return (
+		<CenterComponents>
+			<Logo192 />
+			<CircularProgress />
+			<Typography>{`Game ${game.id} is loading...`}</Typography>
+		</CenterComponents>
+	)
+}
 
 const GamePlay: React.FC<{ game: GameState }> = ({ game }) => {
 	return (
 		<>
 			<Typography>{`Playing game ${game.id}`}</Typography>
-			{/* <PhaserComponent /> */}
+			<Suspense fallback=<LoadingGame game={game} />>
+				<PhaserComponent />
+			</Suspense>
 		</>
 	)
 }
--- app/components/PhaserComponent.tsx
+++ app/components/PhaserComponent.tsx
@@ -1,13 +1,9 @@
-import { Button, Typography } from "@mui/material"
+import { Button } from "@mui/material"
 import { useRef } from "react"
 import { type IRefPhaserGame, PhaserGame } from "~/game/PhaserGame"
-import { useMounted } from "~/hooks/useMounted"
 
 const PhaserComponent: React.FC = () => {
-	const mounted = useMounted()
 	const phaserRef = useRef<IRefPhaserGame | null>(null)
-
-	if (!mounted) return <Typography>Not mounted yet...</Typography>
 
 	const toggleFullScreen = () => {
 		phaserRef.current?.game?.scale.toggleFullscreen()
