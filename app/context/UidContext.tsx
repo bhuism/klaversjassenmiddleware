ca47126dacbbe4b05f5eb5a82152835b91ce1a1a
Bas Huisman <bhuism@odee.net>
2025-06-13 19:50:41 +0200

    cleanups

--- app/context/UidContext.tsx
+++ app/context/UidContext.tsx
@@ -3,19 +3,8 @@ import type { User } from ".generated-sources/openapi"
 
 interface UidContextProps {
 	user?: User
-	deleteUser: (deleterUid: string) => Promise<void>
-	addFeedBack: (comment: string) => Promise<void>
-	addFriend: (adder: string) => Promise<void>
-	delFriend: (remover: string) => Promise<void>
-	//	sendMessage: (messageType: MessageType, receivers: string[], body?: string) => Promise<void>
 }
 
-const UidContext = React.createContext<UidContextProps>({
-	deleteUser: () => Promise.resolve(),
-	addFeedBack: () => Promise.resolve(),
-	addFriend: () => Promise.resolve(),
-	delFriend: () => Promise.resolve(),
-	//sendMessage: () => Promise.resolve(),
-})
+const UidContext = React.createContext<UidContextProps>({})
 
 export default UidContext
--- app/provider/UidContextProvider.tsx
+++ app/provider/UidContextProvider.tsx
@@ -6,30 +6,6 @@ import type { User } from ".generated-sources/openapi"
 
 export const LOCAL_STORAGE_USERID_KEY = "CardSeverAuthUserId"
 
-// export const sendMessageRaw = (
-// 	uid: string,
-// 	messageType: MessageType,
-// 	receivers: string[],
-// 	body?: string
-// ): Promise<void> => {
-// 	const batch = writeBatch(firestore)
-
-// 	receivers.forEach((receiver) => {
-// 		const message: Message = {
-// 			created: Timestamp.now(),
-// 			creator: uid,
-// 			messageType: messageType,
-// 			receiver: receiver,
-// 			read: false,
-// 			body: body,
-// 		}
-
-// 		batch.set(doc(collection(firestore, "messages")).withConverter(messageConverter), message)
-// 	})
-
-// 	return batch.commit().catch((err: Error) => toast.error(err.name + " : " + err.message)) as Promise<void>
-// }
-
 const UidContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
 	let user: User | undefined = undefined
 
@@ -52,54 +28,10 @@ const UidContextProvider: React.FC<React.PropsWithChildren> = ({ children }) => 
 		)
 	}
 
-	// all good
-
-	// const deleteUser = (deleterUid: string): Promise<void> => {
-	// 	return deleteDoc(doc(firestore, "users", deleterUid))
-	// }
-
-	// const addFeedBack = (comment: string): Promise<void> => {
-	// 	return addDoc(collection(firestore, "feedback"), {
-	// 		creator: uid,
-	// 		created: serverTimestamp(),
-	// 		comment: comment,
-	// 	}).catch((err) => {
-	// 		toast.error(err.message)
-	// 	}) as Promise<void>
-	// }
-
-	// const addFriend = (adder: string): Promise<void> => {
-	// 	return updateDoc(doc(firestore, "users", uid), {
-	// 		updated: serverTimestamp(),
-	// 		invites: arrayUnion(adder),
-	// 	})
-	// 		.then(() => {
-	// 			logEvent(analytics, "join_group")
-	// 			sendMessage(MessageType.invitation, [adder])
-	// 		})
-	// 		.catch((err: Error) => toast.error(err.name + " : " + err.message)) as Promise<void>
-	// }
-
-	// const delFriend = (remover: string): Promise<void> => {
-	// 	return updateDoc(doc(firestore, "users", uid), {
-	// 		updated: serverTimestamp(),
-	// 		invites: arrayRemove(remover),
-	// 	}).catch((err: Error) => toast.error(err.name + " : " + err.message)) as Promise<void>
-	// }
-
-	// const sendMessage = (messageType: MessageType, receivers: string[], body?: string): Promise<void> => {
-	// 	return sendMessageRaw(firestore, uid, messageType, receivers, body)
-	// }
-
 	return (
 		<UidContext.Provider
 			value={{
 				user,
-				deleteUser: () => Promise.resolve(),
-				addFeedBack: () => Promise.resolve(),
-				addFriend: () => Promise.resolve(),
-				delFriend: () => Promise.resolve(),
-				//				sendMessage: () => Promise.resolve(),
 			}}
 		>
 			{children}
