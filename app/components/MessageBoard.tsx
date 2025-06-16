816c4c10bcc5a68d8b517d8cd03b2ee5a8584bb0
Bas Huisman <bhuism@odee.net>
2025-06-16 18:09:29 +0200

    better message board

--- app/components/MessageBoard.tsx
+++ app/components/MessageBoard.tsx
@@ -1,34 +1,48 @@
-import { Stack, TextField } from "@mui/material"
-import { useState } from "react"
+import { Button, Grid, TextField } from "@mui/material"
+import React, { useState } from "react"
 import useCardApi from "~/hooks/useGameApi"
 
 const MessageBoard: React.FC = () => {
 	const cardApi = useCardApi()
 	const [message, setMessage] = useState<string>("")
+	const ref = React.createRef<HTMLDivElement>()
+
+	const send = () => {
+		if (message && message.length > 0) {
+			cardApi.sendAMesage({ message }).finally(() => {
+				setMessage("")
+				ref.current?.focus()
+			})
+		}
+	}
 
 	const onKeyPress = (e: React.KeyboardEvent<HTMLDivElement>) => {
 		if (e.key === "Enter") {
 			e.preventDefault()
-			if (message && message.length > 0) {
-				cardApi.sendAMesage({ message })
-			}
-			setMessage("")
+			send()
 		}
 	}
 
 	return (
 		<div>
-			<Stack>
-				<TextField
-					id="outlined-basic"
-					label="Outlined"
-					value={message}
-					variant="outlined"
-					onKeyDown={onKeyPress}
-					onChange={(e) => setMessage(e.target.value)}
-					autoFocus
-				/>
-			</Stack>
+			<Grid container spacing={2}>
+				<Grid size={11}>
+					<TextField
+						inputRef={ref}
+						fullWidth
+						value={message}
+						variant="outlined"
+						onKeyDown={onKeyPress}
+						onChange={(e) => setMessage(e.target.value)}
+						autoFocus
+					/>
+				</Grid>
+				<Grid size={1}>
+					<Button variant="outlined" onClick={send}>
+						Send
+					</Button>
+				</Grid>
+			</Grid>
 		</div>
 	)
 }
